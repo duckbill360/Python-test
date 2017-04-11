e6f6009f75a8d2759560c341fb63f977ed3124ea
Po-Wei Cheng <duckbill360@users.noreply.github.com>
2017-04-11 23:09:31 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,20 @@
+import math
+import timeit
+
+
+def nCr(n,r):
+    f = math.factorial
+    return f(n) // f(r) // f(n-r)
+
+if __name__ == '__main__':
+
+    start = timeit.default_timer()
+
+    num = 0
+    for i in range(1000000000):
+        num += i
+    print(num)
+
+    stop = timeit.default_timer()
+    
+    print(stop - start, 'sec')
